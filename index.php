f1dba492a61e0e4c2762123a9d03e55735112c5c
Redes <redes@Info01-06.(none)>
2015-11-03 16:44:17 -0200

    adicionado index.php

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 require 'vendor/autoload.php';
+
 $app = new \Slim\Slim();
+
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+    $inteiro1 = 42;
+    $inteiro2 = 79;
+    $texto = "Oĺá, ";
+    $fracao = 2.3;
+    
+    $soma = $inteiro1 + $inteiro2;
+    $divisao = $inteiro1 / $inteiro2;
+    $multi =  $inteiro1 * $inteiro2;
+    $sub =  $inteiro1 - $inteiro2;
+    $olaMundo = $texto . $name;
+    
+    echo "<p>Soma: " . $soma . "</p>";
+    echo "<p>Divisão: " . $divisao . "</p>";
+    echo "<p>Multiplica: " . $multi . "</p>";
+    echo "<p>Subtração: " . $sub . "</p>";
+    echo "<p>" . $olaMundo . "</p>";
+    
+    header('Content-Type: text/html; charset=UTF-8');
+    echo '<html>';
+    
 });
+
 $app->run();
